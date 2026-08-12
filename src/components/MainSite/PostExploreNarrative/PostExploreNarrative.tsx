6bfc7bb710a37d8446e652f033d74e281472893e
manbtd0-cloud <manbtd0@gmail.com>
2026-08-12 06:55:28 -0700

    feat: mount document-space journey narrative

--- src/components/MainSite/PostExploreNarrative/PostExploreNarrative.tsx
+++ src/components/MainSite/PostExploreNarrative/PostExploreNarrative.tsx
@@ -1,13 +1,13 @@
 import { postExploreCopy } from '@/content/homepage';
 import { AuroraStatement } from './AuroraStatement';
 import { GrowthRing } from './GrowthRing';
-import { TrailNarrative } from './TrailNarrative';
+import { JourneyNarrative } from './JourneyNarrative';
 import styles from './PostExploreNarrative.module.css';
 
 export function PostExploreNarrative() {
   return (
     <section id="post-explore" className={styles.root} data-post-explore-narrative>
-      <TrailNarrative
+      <JourneyNarrative
         questions={postExploreCopy.questions}
         reassurance={postExploreCopy.reassurance}
       />
